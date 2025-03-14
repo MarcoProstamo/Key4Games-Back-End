fc156d2d62075534db9f3d9538c5ff2b28c04517
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-14 14:04:56 +0100

    Add → Games List + Prepared Navigation Btns

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -12,45 +12,94 @@ class GameSeeder extends Seeder
         $games = [
             [
                 "title" => "The Last of Us Part II",
-                "description" => "The Last of Us Part II is an action-adventure game developed by Naughty Dog and published by Sony Interactive Entertainment.",
+                "description" => "An action-adventure game developed by Naughty Dog and published by Sony Interactive Entertainment.",
                 "developer" => "Naughty Dog",
                 "publisher" => "Sony Interactive Entertainment",
-                "genre_id" => 2 // Adventure
+                "genre_id" => 2
             ],
             [
                 "title" => "Cyberpunk 2077",
-                "description" => "Cyberpunk 2077 is an open-world RPG developed by CD Projekt Red.",
+                "description" => "An open-world RPG developed by CD Projekt Red.",
                 "developer" => "CD Projekt Red",
                 "publisher" => "CD Projekt",
-                "genre_id" => 3 // RPG
+                "genre_id" => 3
             ],
             [
                 "title" => "Call of Duty: Warzone",
-                "description" => "Call of Duty: Warzone is a free-to-play battle royale game developed by Infinity Ward and Raven Software.",
+                "description" => "A free-to-play battle royale game developed by Infinity Ward and Raven Software.",
                 "developer" => "Infinity Ward, Raven Software",
                 "publisher" => "Activision",
-                "genre_id" => 10 // Battle Royale
+                "genre_id" => 10
             ],
             [
                 "title" => "Dark Souls III",
-                "description" => "Dark Souls III is an action RPG developed by FromSoftware and published by Bandai Namco Entertainment.",
+                "description" => "An action RPG developed by FromSoftware and published by Bandai Namco Entertainment.",
                 "developer" => "FromSoftware",
                 "publisher" => "Bandai Namco Entertainment",
-                "genre_id" => 3 // RPG
+                "genre_id" => 3
             ],
             [
                 "title" => "League of Legends",
-                "description" => "League of Legends is a competitive MOBA developed and published by Riot Games.",
+                "description" => "A competitive MOBA developed and published by Riot Games.",
                 "developer" => "Riot Games",
                 "publisher" => "Riot Games",
-                "genre_id" => 11 // MOBA
+                "genre_id" => 11
             ],
             [
                 "title" => "Resident Evil Village",
-                "description" => "Resident Evil Village is a survival horror game developed and published by Capcom.",
+                "description" => "A survival horror game developed and published by Capcom.",
                 "developer" => "Capcom",
                 "publisher" => "Capcom",
-                "genre_id" => 27 // Horror
+                "genre_id" => 27
+            ],
+            [
+                "title" => "Elden Ring",
+                "description" => "An action RPG by FromSoftware, directed by Hidetaka Miyazaki and with world-building by George R. R. Martin.",
+                "developer" => "FromSoftware",
+                "publisher" => "Bandai Namco Entertainment",
+                "genre_id" => 3
+            ],
+            [
+                "title" => "God of War Ragnarok",
+                "description" => "The sequel to God of War (2018), following Kratos and Atreus in Norse mythology.",
+                "developer" => "Santa Monica Studio",
+                "publisher" => "Sony Interactive Entertainment",
+                "genre_id" => 2
+            ],
+            [
+                "title" => "Hollow Knight",
+                "description" => "A Metroidvania-style action-adventure game developed by Team Cherry.",
+                "developer" => "Team Cherry",
+                "publisher" => "Team Cherry",
+                "genre_id" => 9
+            ],
+            [
+                "title" => "Red Dead Redemption 2",
+                "description" => "An open-world action-adventure game set in the Wild West, developed by Rockstar Games.",
+                "developer" => "Rockstar Games",
+                "publisher" => "Rockstar Games",
+                "genre_id" => 2
+            ],
+            [
+                "title" => "Minecraft",
+                "description" => "A sandbox game where players can build and explore infinite worlds, developed by Mojang Studios.",
+                "developer" => "Mojang Studios",
+                "publisher" => "Microsoft",
+                "genre_id" => 19
+            ],
+            [
+                "title" => "The Witcher 3: Wild Hunt",
+                "description" => "An open-world action RPG by CD Projekt Red, following the story of Geralt of Rivia.",
+                "developer" => "CD Projekt Red",
+                "publisher" => "CD Projekt",
+                "genre_id" => 3
+            ],
+            [
+                "title" => "Super Mario Odyssey",
+                "description" => "A 3D platformer featuring Mario exploring various kingdoms with his sentient hat, Cappy.",
+                "developer" => "Nintendo",
+                "publisher" => "Nintendo",
+                "genre_id" => 8
             ]
         ];
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <title>Document</title>
+    <title>Games List</title>
 </head>
 
 <body>
-    <h1 class="text-center">Ciao!</h1>
+    <div class="container my-5">
+        <h1 class="text-center mb-3 fw-semibold">GAMES LIST</h1>
+        <div class="list-group">
+            <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                <div>
+                    New Game Title...
+                </div>
+                <div>
+                    <button class="btn btn-success fw-bold text-light px-4 me-5">ADD ➕</button>
+                </div>
+            </div>
+            @foreach ($games as $game)
+                <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                    <div>
+                        {{ $game['title'] }}
+                    </div>
+                    <div>
+                        <button class="btn btn-warning fw-bold text-dark me-2">EDIT 🖋️</button>
+                        <button class="btn btn-danger fw-bold text-dark">DELETE 🗑️</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </body>
 
 </html>
