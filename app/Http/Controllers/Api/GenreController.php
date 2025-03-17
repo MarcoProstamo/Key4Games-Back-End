aad7e1ee43c954dbdd83348695e9b342de1241d9
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-17 11:55:06 +0100

    Add → Genre API

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -3,63 +3,39 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Genre;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $genres = Genre::all();
+        return response()->json($genres);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $genre = Genre::create(attributes: $data);
+        return response()->json($genre);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(Genre $genre)
     {
-        //
+        return response()->json($genre);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function update(Request $request, Genre $genre)
     {
-        //
+        $data = $request->all();
+        $genre->update(attributes: $data);
+        return response()->json($genre);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy(Genre $genre)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $genre->delete();
+        return response()->json("Deleted Successfully");
     }
 }
