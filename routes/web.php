38526e8cd1461f2d5e9ccbdd0369974ee8e4d3fa
MarcoProstamo <Marcopros1201@gmail.com>
2025-03-21 12:20:09 +0100

    Add → Auth to Database Manager (Removed Registration)

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,25 @@
 
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function () {
-    return view("pages/home");
-})->name("home");
+Route::get('/', function () {
+    return view('/pages/home');
+})->name('home')->middleware(["auth", "verified"]);
 
-Route::resource("games", GameController::class);
-Route::resource("genres", GenreController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource("/games", GameController::class)->middleware(['auth', 'verified']);
+Route::resource("/genres", GenreController::class)->middleware(['auth', 'verified']);
+
+
+require __DIR__ . '/auth.php';
